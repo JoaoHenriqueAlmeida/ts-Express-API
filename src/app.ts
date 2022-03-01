2c639729de1930dca579d2ca33c6c5eac0e94129
JoaoHenriqueAlmeida <joaoh.almeidaaraujo@gmail.com>
2022-03-01 10:01:02 -0300

    Impl productsRouter

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import userRouter from './routes/userRouter';
+import productsRouter from './routes/productsRouter';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/', userRouter);
+app.use('/', productsRouter);
 
 export default app;
